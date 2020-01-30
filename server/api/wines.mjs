28cf321efdf2f81d93f9d2691025d5b32a3f4664
paulluhrsen <paulluhrsen@gmail.com>
2020-01-30 15:54:07 -0600

    single wine express route working

--- server/api/wines.mjs
+++ server/api/wines.mjs
@@ -1,20 +1,35 @@
-import express from 'express'
-import { driver } from '../index.mjs'
+import express from "express";
+import { driver } from "../index.mjs";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res) => {
-  let session
+router.get("/", async (req, res) => {
+  let session;
   try {
-    session = driver.session()
-    const cypher = 'MATCH (n:Wine) RETURN n LIMIT 100'
-    const { records } = await session.run(cypher)
-    res.send(records)
+    session = driver.session();
+    const cypher = "MATCH (n:Wine) RETURN n LIMIT 100";
+    const { records } = await session.run(cypher);
+    res.send(records);
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   } finally {
-    await session.close()
+    await session.close();
   }
-})
+});
 
-export default router
+router.get("/:wineId", async (req, res) => {
+  let session;
+  try {
+    session = driver.session();
+    const wineId = req.params.wineId;
+    const cypher = `MATCH (n:Wine) WHERE ID(n) = ${wineId} RETURN n`;
+    const { records } = await session.run(cypher);
+    res.send(records);
+  } catch (err) {
+    res.status(500).send(err);
+  } finally {
+    await session.close();
+  }
+});
+
+export default router;
