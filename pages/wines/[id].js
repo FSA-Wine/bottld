0e2bdd44ea7ae8d5f99ac04e854edb746467c62f
paulluhrsen <paulluhrsen@gmail.com>
2020-01-31 10:10:19 -0600

    getting correct data from server

--- pages/wines/[id].js
+++ pages/wines/[id].js
@@ -1,43 +1,51 @@
-import { useRouter } from 'next/router'
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import fetchSingleWine from '../../store/singleWine'
+import { useRouter } from "next/router";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchSingleWine } from "../../store/singleWine";
 
-class SingleWine extends Component {
+const SingleWine = props => {
+  const router = useRouter();
 
-  constructor() {
-    super()
-    const router = useRouter()
+  return <SingleWineWithoutRouter {...props} router={router} />;
+};
+
+class SingleWineWithoutRouter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loc: props.router.query.loc,
+      loaded: false
+    };
   }
 
   componentDidMount() {
-    //this.props.fetchSingleWine(router.query.id)
-    console.log(router.query.id)
+    this.props.fetchSingleWine(this.props.router.query.id);
+    console.log("this?", this);
+    console.log("props?", this.props);
   }
 
   render() {
-
     return (
       <div>
-        <h1>Wine #{router.query.id}</h1>
+        <h1>Wine #{this.props.router.query.id}</h1>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     singleWine: state.singleWine
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
     fetchSingleWine: id => dispatch(fetchSingleWine(id))
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(SingleWine)
+export default connect(mapStateToProps, mapDispatchToProps)(SingleWine);
 
 // export default () => {
 //   const router = useRouter()
@@ -48,5 +56,3 @@ export default connect(mapStateToProps, mapDispatchToProps)(SingleWine)
 //     </>
 //   )
 // }
-
-
