d03ca0bf8ee767e95e14a52e87cebab6837a01d2
paulluhrsen <paulluhrsen@gmail.com>
2020-02-04 17:28:38 -0600

    map data correct but not displaying correct label

--- components/SingleWineCharts.js
+++ components/SingleWineCharts.js
@@ -1,77 +1,91 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import { render } from '@testing-library/react';
-import SingleWineMap from './SingleWineMap'
-import PolarGraph from './PolarGraph'
+import SingleWineMap from "./SingleWineMap";
+import PolarGraph from "./PolarGraph";
+
+let backgroundColorData = [
+  "rgba(89, 110, 113, .5)",
+  "rgba(97, 116, 71, .5)",
+  "rgba(171, 170, 139, .5)",
+  "rgba(230, 221, 152, .5)",
+  "rgba(178, 157, 100, .5)"
+];
+let labelData = [];
+let fakelabelData = ["Nutty", "Fruit", "Spice", "Body", "Caramel"];
+let fakeNumData = [10, 8, 7, 3, 2];
+let numData = [];
 
 class SingleWineCharts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            chartData: {},
-            singleWine: [{
-                _fields: [{
-                    properties: {
-                        country: "France", province: "Champagne", variety: "Champagne Blend", description: "This is a full in the mouth, forward and fruity rosé, that is all fresh acidity and lively, crisp flavors. It has a light, zesty texture and the merest hint of toastiness. Drink now or give it a year's bottle aging.",
-                        winery: "Dampierre",
-                        title: "Dampierre NV Cuvée des Ambassadeurs Brut Rosé (Champagne)"
-                    },
-                    points: { low: 91, high: 0 }
-                }]
-            }]
-        }
-    }
-
-    componentDidMount() {
-        //this.props.fetchSingleWine(this.props.router.query.id);
-        this.getChartData()
-    }
-
-    getChartData() {
-        this.setState({
-            chartData: {
-                labels: [
-                    "Nutty",
-                    "Fruit",
-                    "Spice",
-                    "Body",
-                    "Caramel"
-                ],
-                datasets: [{
-                    data: [
-                        10,
-                        8,
-                        7,
-                        3,
-                        2
-                    ],
-                    backgroundColor: [
-                        "rgba(89, 110, 113, .5)",
-                        "rgba(97, 116, 71, .5)",
-                        "rgba(171, 170, 139, .5)",
-                        "rgba(230, 221, 152, .5)",
-                        "rgba(178, 157, 100, .5)"
-                    ],
-                }]
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: {
+        labels: [],
+        datasets: [
+          {
+            data: [],
+            backgroundColor: backgroundColorData
+          }
+        ]
+      },
+      singleWine: [
+        {
+          _fields: [
+            {
+              properties: {
+                country: "France",
+                province: "Champagne",
+                variety: "Champagne Blend",
+                description:
+                  "This is a full in the mouth, forward and fruity rosé, that is all fresh acidity and lively, crisp flavors. It has a light, zesty texture and the merest hint of toastiness. Drink now or give it a year's bottle aging.",
+                winery: "Dampierre",
+                title:
+                  "Dampierre NV Cuvée des Ambassadeurs Brut Rosé (Champagne)"
+              },
+              points: { low: 91, high: 0 }
             }
-        })
-    }
+          ]
+        }
+      ]
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                <div>
-                    <SingleWineMap />
-                </div>
-                <div className="chart-container">
-                    <p className='sm-gray'>FLAVOR PROFILE</p>
-                    <div className='polar-container' >
-                        <PolarGraph chartData={this.state.chartData} />
-                    </div>
-                </div>
-            </div>
-        )
+  componentDidMount() {
+    let charData = this.props.flavorData;
+    if (charData) {
+      charData.map(el => {
+        labelData.push(el._fields[0]);
+        numData.push(el._fields[1].low);
+      });
     }
+    console.log("char?", charData);
+    console.log("data?", labelData);
+    this.getChartData();
+  }
 
+  getChartData() {
+    let chartData = { ...this.state.chartData };
+    chartData.labels = labelData;
+    chartData.datasets[0].data = numData;
+    this.setState({ chartData });
+  }
+
+  render() {
+    console.log("flavordata?", this.state);
+    return (
+      <div>
+        <div>
+          <SingleWineMap />
+        </div>
+        <div className="chart-container">
+          <p className="sm-gray">FLAVOR PROFILE</p>
+          <div className="polar-container">
+            <PolarGraph chartData={this.state.chartData} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SingleWineCharts;
--- pages/wines/[id].js
+++ pages/wines/[id].js
@@ -6,9 +6,9 @@ import { fetchSingleWine } from "../../store/singleWine";
 import "semantic-ui-css/semantic.min.css";
 import { Grid, Image } from "semantic-ui-react";
 import Layout from "../../components/Layout";
-import SingleWineDetails from '../../components/SingleWineDetails'
-import SingleWineCharts from '../../components/SingleWineCharts'
-import RecommendRow from '../../components/RecommendRow'
+import SingleWineDetails from "../../components/SingleWineDetails";
+import SingleWineCharts from "../../components/SingleWineCharts";
+import RecommendRow from "../../components/RecommendRow";
 
 const SingleWine = props => {
   const router = useRouter();
@@ -29,41 +29,56 @@ class SingleWineWithoutRouter extends Component {
     this.props.fetchSingleWine(this.props.router.query.id);
   }
 
-
   render() {
     const singleWine = this.props.singleWine;
 
     if (singleWine.length) {
-      let recWineArr = this.props.singleWine[1]
+      let flavorData = this.props.singleWine[2];
+      let recWineArr = this.props.singleWine[1];
       let curWine = singleWine[0][0]._fields[0].properties;
       return (
         <Layout>
-          <div className='content smaller-text'>
-            <div className='top-section'>
-
-              <Grid centered stackable doubling columns={3} style={{ margin: `35px` }}>
-                <Grid.Column width={2} only='tablet computer'>
-                  <Image centered src='/wineRed.png' alt="red wine" style={{ width: `120px`, top: `9px` }} />
+          <div className="content smaller-text">
+            <div className="top-section">
+              <Grid
+                centered
+                stackable
+                doubling
+                columns={3}
+                style={{ margin: `35px` }}
+              >
+                <Grid.Column width={2} only="tablet computer">
+                  <Image
+                    centered
+                    src="/wineRed.png"
+                    alt="red wine"
+                    style={{ width: `120px`, top: `9px` }}
+                  />
                 </Grid.Column>
                 <Grid.Column width={8}>
                   <SingleWineDetails singleWine={curWine} />
                 </Grid.Column>
-                <Grid.Column doubling stackable mobile={16} tablet={16} computer={6}>
-                  <SingleWineCharts />
+                <Grid.Column
+                  doubling
+                  stackable
+                  mobile={16}
+                  tablet={16}
+                  computer={6}
+                >
+                  <SingleWineCharts flavorData={flavorData} />
                 </Grid.Column>
               </Grid>
             </div>
-            <div className='lower-section-bg'>
-              <div className='lower-section' >
-                <Grid >
+            <div className="lower-section-bg">
+              <div className="lower-section">
+                <Grid>
                   <Grid.Row>
                     <RecommendRow recWines={recWineArr} />
                   </Grid.Row>
                 </Grid>
               </div>
             </div>
-
-          </div >
+          </div>
         </Layout>
       );
     } else {
