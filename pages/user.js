cf7fabc76ffcd9cd09c12f85fce952bc4d17777d
Kevin Thai <kevin.k.thai@gmail.com>
2020-02-06 12:19:46 -0600

    Added user validation for profile page

--- pages/user.js
+++ pages/user.js
@@ -13,54 +13,61 @@ const User = props => {
   const [view, setView] = useState('likedWines')
   return (
     <Layout>
-      <section>
-        <section className="wines">
-          <h2>Cheers, {props.user.firstName}!</h2>
-          <button onClick={() => setView('likedWines')}>Liked</button>
-          <span>
-            <button onClick={() => setView('triedWines')}>Tried</button>
-          </span>
-          <ul>
-            {!props.user[view] ? (
-              <li>Nothing!</li>
-            ) : (
-              props.user[view].map(wine => {
-                const wineProps = wine._fields[0].properties
-                return (
-                  <div>
-                    <Link
-                      href="/wines/[id]"
-                      as={`/wines/${wineProps.id.low}`}
-                      key={wineProps.id.low}>
-                      <a className="card">
-                        <li>{wineProps.title}</li>
-                      </a>
-                    </Link>
-                  </div>
-                )
-              })
-            )}
-          </ul>
-          <div>
-            {!props.user[view] ? (
-              <div>Flavor Profile Not Available: Like some wines to get it started!</div>
-            ) : (
-              <div>
-                <DynamicGraph likedWine={props.user.likedWines} />
-              </div>
-            )}
-          </div>
+      {props.isLoggedIn ? (
+        <section>
+          <section className="wines">
+            <h2>Cheers, {props.user.firstName}!</h2>
+            <button onClick={() => setView('likedWines')}>Liked</button>
+            <span>
+              <button onClick={() => setView('triedWines')}>Tried</button>
+            </span>
+            <ul>
+              {!props.user[view] ? (
+                <li>Nothing!</li>
+              ) : (
+                props.user[view].map(wine => {
+                  const wineProps = wine._fields[0].properties
+                  return (
+                    <div>
+                      <Link
+                        href="/wines/[id]"
+                        as={`/wines/${wineProps.id.low}`}
+                        key={wineProps.id.low}>
+                        <a className="card">
+                          <li>{wineProps.title}</li>
+                        </a>
+                      </Link>
+                    </div>
+                  )
+                })
+              )}
+            </ul>
+            <div>
+              {!props.user[view] ? (
+                <div>Flavor Profile Not Available: Like some wines to get it started!</div>
+              ) : (
+                <div>
+                  <DynamicGraph likedWine={props.user.likedWines} />
+                </div>
+              )}
+            </div>
+          </section>
+          <section className="data">
+            <div></div>
+          </section>
         </section>
-        <section className="data">
-          <div></div>
+      ) : (
+        <section>
+          <h2>Access Denied: Please log in first</h2>
         </section>
-      </section>
+      )}
     </Layout>
   )
 }
 
 const mapState = state => ({
   user: state.user,
+  isLoggedIn: state.user.googleId,
 })
 
 export default connect(mapState)(User)
