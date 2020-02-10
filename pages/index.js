6f75dbfe367f7aed2509055a10c6efd368c97adb
Kristen Andersen <kristen.andersen@gmail.com>
2020-02-10 14:41:36 -0600

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import Head from 'next/head'
 import Link from 'next/link'
 import Router from 'next/router'
-import { Button, Grid, Input, Segment } from 'semantic-ui-react'
+import { Button, Grid, Input } from 'semantic-ui-react'
 import Layout from '../components/Layout'
 
 //sign in button hidden if logged in
@@ -30,27 +30,46 @@ const Home = props => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <Grid id="carousel">
-          <Grid.Column id="carousel-text">
+        <Grid id="carousel" style={{ width: `80vw`, margin: `0 auto` }} verticalAlign="middle">
+          <Grid.Column id="carousel-text" doubling centered width={12}>
             <Grid.Row>
-              <h1 style={{ color: `#980000` }}>Find your perfect wine match.</h1>
-              <h4 style={{ fontStyle: `italic`, letterSpacing: `.1px` }}>
-                {`Enter the name of a wine you've enjoyed, and we'll recommend similar wines.`}
+              <h1
+                style={{
+                  color: `#980000`,
+                  fontFamily: `Playfair Display, serif`,
+                  fontSize: `2.9rem`,
+                }}>
+                Find your perfect wine match.
+              </h1>
+              <h4
+                style={{
+                  fontStyle: `italic`,
+                  letterSpacing: `.1px`,
+                  fontSize: `1.5rem`,
+                  fontFamily: `Lato, serif`,
+                  fontWeight: `400`,
+                }}>
+                {`Enter the name of a wine you've enjoyed, `}
+                <br />
+                {`and we'll recommend
+            similar wines.`}
               </h4>
             </Grid.Row>
-            <Segment>
-              <Grid.Row style={{ margin: `20px` }}>
-                <Input
-                  type="text"
-                  name="search"
-                  value={search}
-                  onChange={handleChange}
-                  placeholder="Enter a wine name"></Input>
-                <Button type="submit" style={{ margin: `10px` }} onClick={handleSubmit}>
-                  Submit
-                </Button>
-              </Grid.Row>
-            </Segment>
+
+            <Grid.Row style={{ margin: `20px` }}>
+              <Input
+                type="text"
+                name="search"
+                value={search}
+                onChange={handleChange}
+                placeholder="Enter a wine name"></Input>
+              <Button
+                type="submit"
+                style={{ margin: `10px`, backgroundColor: `#b7b7b7ff` }}
+                onClick={handleSubmit}>
+                Submit
+              </Button>
+            </Grid.Row>
 
             <Grid.Row>
               <h4>
@@ -66,10 +85,21 @@ const Home = props => {
                     SIGN IN
                   </a>
                 </Button>{' '}
-                for personalized recommendations
+                <span style={{ fontWeight: `400` }}>for personalized recommendations</span>
               </h4>
             </Grid.Row>
           </Grid.Column>
+          <Grid.Row style={{ height: `80vh` }}>
+            <Grid.Column only="tablet computer" width={5} floated="right">
+              <img
+                alt="wine illustration"
+                src="/bottles-outline-glass-rev.svg"
+                style={{
+                  opacity: `60%`,
+                }}
+              />
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
       </div>
     </Layout>
