edad1d548e47a686c9c09caaf4ba979373e095b1
Kevin Thai <kevin.k.thai@gmail.com>
2020-02-11 15:13:52 -0600

    Fixed search query cypher, added a search bar to the search results page

--- components/UserWineList.js
+++ components/UserWineList.js
@@ -2,57 +2,38 @@ import React from 'react'
 import { Card, List } from 'semantic-ui-react'
 import WineListItem from './WineListItem'
 
-export default ({ wines, view }) => {
-  if (wines.length > 4) {
-    return (
-      <Card className="lists" style={{ width: `100%` }}>
-        <List divided selection relaxed style={{ padding: `14px, 0` }}>
-          {wines.map((wine, i) => {
-            return (
-              <div key={i}>
-                <WineListItem
-                  wineProps={wine._fields[0].properties}
-                  liked={view === 'likedWines' ? true : false}
-                />
-              </div>
-            )
-          })}
-        </List>
-      </Card>
-    )
-  } else if (wines.length && wines.length < 5) {
-    return (
-      <Card className="lists" style={{ width: `100%` }}>
-        <List divided selection relaxed style={{ padding: `14px, 0` }}>
-          {wines.map((wine, i) => {
-            return (
-              <div key={i}>
-                <WineListItem
-                  wineProps={wine._fields[0].properties}
-                  liked={view === 'likedWines' ? true : false}
-                />
-              </div>
-            )
-          })}
-          <div>
-            <h3
-              style={{
-                fontStyle: `italic`,
-                marginBottom: `20px`,
-                marginTop: `45%`,
-                textAlign: `center`,
-              }}>
-              {`Like ${5 - wines.length} more wines to improve your recommendations!`}
-            </h3>
+export default ({ wines, view }) => (
+  <Card className="lists" style={{ width: `100%` }}>
+    <List divided selection relaxed style={{ padding: `14px, 0` }}>
+      {wines.map((wine, i) => {
+        return (
+          <div key={i}>
+            <WineListItem
+              wineProps={wine._fields[0].properties}
+              liked={view === 'likedWines' ? true : false}
+            />
           </div>
-        </List>
-      </Card>
-    )
-  } else {
-    return (
+        )
+      })}
+      {/* {wines.lengthwines.length < 5 ? ( */}
       <div>
-        <h3 style={{ fontStyle: `italic` }}>Like some wines to get started!</h3>
+        <h3
+          style={{
+            fontStyle: `italic`,
+            marginBottom: `15px`,
+            marginTop: `15px`,
+            textAlign: `center`,
+          }}>
+          {!wines.length
+            ? `Like some wines to get started!`
+            : wines.length < 5
+            ? `Like ${5 - wines.length} more wines to improve your recommendations!`
+            : ''}
+        </h3>
       </div>
-    )
-  }
-}
+      {/* ) : (
+        <></>
+      )} */}
+    </List>
+  </Card>
+)
--- pages/wines/index.js
+++ pages/wines/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import Head from 'next/head'
 import Link from 'next/link'
-import { Dropdown, Segment, Grid } from 'semantic-ui-react'
+import { Dropdown, Segment, Grid, Input } from 'semantic-ui-react'
 import { useRouter } from 'next/router'
 import { fetchWines } from '../../store/wines'
 import Paginate from '../../components/Paginate'
@@ -19,29 +19,37 @@ import {
 import ErrorNoResults from '../../components/ErrorNoResults'
 
 const Wines = props => {
+  const router = useRouter()
+  const [search, setSearch] = useState(router.query.search)
   const [page, setPage] = useState(1)
   const [limit, setLimit] = useState(25)
   const [color, setColor] = useState('')
-  const [vintage, setVintage] = useState('')
   const [country, setCountry] = useState('')
   const [variety, setVariety] = useState('')
   const [priceLow, setPriceLow] = useState(0)
   const [priceHigh, setPriceHigh] = useState(99999)
   const [view, setView] = useState('likedWines')
 
-  const router = useRouter()
-
   useEffect(() => {
     props.fetchWines(page, limit, {
-      value: router.query.search,
+      value: search,
       color,
       country,
       variety,
       priceHigh,
       priceLow,
-      vintage,
     })
-  }, [page, limit, router.query.search, color, vintage, country, variety, priceHigh, priceLow])
+  }, [page, limit, search, color, country, variety, priceHigh, priceLow])
+
+  // const handleSubmit = e => {
+  //   e.preventDefault()
+  //   Router.push({
+  //     pathname: '/wines',
+  //     as: `/wines?search=${search}`,
+  //     query: { search },
+  //   })
+  // }
+
   return (
     <Layout>
       <div>
@@ -49,14 +57,23 @@ const Wines = props => {
           <title>Search Wines</title>
           <link rel="icon" href="/favicon.ico" />
         </Head> */}
-        <Grid.Row centered columns={5} style={{ margin: `0 0 0 10px` }}>
-          <Dropdown
-            placeholder="Select vintage"
-            clearable
-            search
-            selection
-            onChange={(e, { value }) => setVintage(value)}
+        <Grid.Row style={{ margin: `20px` }}>
+          <Input
+            type="text"
+            name="search"
+            value={search}
+            onChange={(e, { value }) => setSearch(value)}
+            placeholder="Enter a wine name"
           />
+          {/* <Button
+            type="submit"
+            style={{ margin: `10px`, backgroundColor: `#b7b7b7ff` }}
+            disabled={!search}
+            onClick={handleSubmit}>
+            Submit
+          </Button> */}
+        </Grid.Row>
+        <Grid.Row centered columns={5} style={{ margin: `0 0 0 10px` }}>
           <Dropdown
             placeholder="Select Country"
             clearable
